16c28b3ff1fa3a72022d2fc26b1fbf268e940a75
Tyson <tyelf22@hotmail.com>
2018-11-13 23:21:35 -0800

    Refactored JS, css animations

--- pokecode.js
+++ pokecode.js
@@ -3,28 +3,70 @@ import { pokemon } from './pokemon.js'
 console.log(pokemon)
 
 const pokeContainer = document.querySelector('#container')
-let count = 0
 
 pokemon.forEach(poke => {
     let card = document.createElement('div')
-    //console.log(`${poke.id}${poke.ename}.png`)
-    let fig = document.createElement('figure')
+    card.className = "card"
+    card.addEventListener('click', function(){
+        card.classList.toggle('is-flipped');
+    })
+
+    let cardFront = document.createElement('figure')
+    cardFront.className = "cardFaceFront"
+
     let cap = document.createElement('figcaption')
     let img = document.createElement('img')
-    let backDiv = document.createElement('div')
-    backDiv.textContent = poke.cname
-    cap.textContent = poke.ename
+
     img.src = `img/img/${poke.id}${poke.ename}.png`
-    fig.appendChild(img)
-    fig.appendChild(cap)
-    card.appendChild(fig)
-    card.appendChild(backDiv)
-    pokeContainer.appendChild(card) 
-    card.addEventListener( 'click', function() {
-        card.classList.toggle('is-flipped');
-      });
+    cap.textContent = poke.ename
+    cardFront.appendChild(img)
+    cardFront.appendChild(cap)
     
-      //backDiv.addEventListener( 'click', function() {
-          //backDiv.addEventListener('isflipped');
-      //});
-});
+
+    let cardBack = document.createElement('figure')
+    cardBack.className = "cardFaceBack"
+    let capt = document.createElement('figcaption')
+    let backimg = document.createElement('img')
+
+    backimg.src = "img/img/PokemonLogo.png"
+    backimg.className = "backImg"
+    capt.textContent = poke.ename
+    cardBack.appendChild(backimg)
+    cardBack.appendChild(capt)
+    pokeContainer.appendChild(card)
+    card.appendChild(cardFront)
+    card.appendChild(cardBack)
+    
+    
+    //created stats container
+    let statsContainer = document.createElement('div')
+    statsContainer.className = "statsContainer"
+    cardBack.appendChild(statsContainer)
+
+    //adding each stat
+    let attack = document.createElement('p')
+    attack.textContent = `Attack: ${poke.base.Attack}`
+    statsContainer.appendChild(attack)
+
+    let defense = document.createElement('p')
+    defense.textContent = `Defense: ${poke.base.Defense}`
+    statsContainer.appendChild(defense)
+
+    let hp = document.createElement('p')
+    hp.textContent = `HP: ${poke.base.HP}` 
+    statsContainer.appendChild(hp)
+
+    let specialAttack = document.createElement('p')
+    specialAttack.textContent = 'Sp. Attack: '+ poke.base["Sp.Atk"]
+    statsContainer.appendChild(specialAttack)
+
+    let specialDefense = document.createElement('p')
+    specialDefense.textContent = "Sp. Defense: "+ poke.base["Sp.Def"]
+    statsContainer.appendChild(specialDefense)
+
+    let speed = document.createElement('p')
+    speed.textContent = `Speed: ${poke.base.Speed}`
+    statsContainer.appendChild(speed)
+
+    
+})
